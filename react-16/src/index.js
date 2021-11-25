6a019286f95ce0cb1f97a83d4f685b2711c505c5
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-11-25 22:55:50 +0530

    okie

---
+++ react-16/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+/* import "../src/KGF/CSS/style.css"; */
+//ReactDOM.render("What you want to render", "Where you want to render")
+//ReactDOM.render("Hello, Welcome to React", document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
